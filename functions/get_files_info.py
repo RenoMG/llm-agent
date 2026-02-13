9856a1d70a0114fce580961d2d4f061a2bacbb77
RenoMG <reno@renos.world>
2026-02-13 00:22:32 -0800

    list file info in path

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -7,10 +7,19 @@ def get_files_info(working_directory, directory="."):
         valid_target_dir = os.path.commonpath([abs_path, full_path]) == abs_path
 
         if valid_target_dir:
-            print(full_path)
+            get_files = os.listdir(full_path)
+
+            file_info = []
+            for f in get_files:
+                try:
+                    file_info.append(f"- {f}: file_size={os.path.getsize(f"{full_path}/{f}")} bytes, is_dir={os.path.isdir(f)}\n")
+                except Exception as err:
+                    return f"Error: {err}"
+
+            print("".join(file_info))
         else:
             return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     else:
         f'Error: "{directory}" is not a directory'
 
-get_files_info("/home/ubuntu/workspace/bootdotdev/llm-agent")
+get_files_info("/home/ubuntu/workspace/bootdotdev/llm-agent", "calculator")
